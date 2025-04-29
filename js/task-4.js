a8e8eef69bbfb7544959c0627aa3d649a0ca7a91
Wisdom <dezvish@gmail.com>
2025-04-29 20:48:02 +0300

    final-hot-fix

--- js/task-4.js
+++ js/task-4.js
@@ -1,24 +1,23 @@
 const loginForm = document.querySelector('.login-form');
 
-loginForm.addEventListener('submit', handleSubmit);
-
-function handleSubmit(event) {
+loginForm.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  const form = event.target;
-  const email = form.elements.login.value.trim();
-  const password = form.elements.password.value.trim();
+  const { email, password } = loginForm.elements;
 
-  if (email === '' || password === '') {
-    return alert('All form fields must be filled in');
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+    return;
   }
 
   const formData = {
-    email,
-    password,
+    email: emailValue,
+    password: passwordValue,
   };
 
   console.log(formData);
-
-  form.reset();
-}
+  loginForm.reset();
+});
